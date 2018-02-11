7c1d9014453ccee8f471fc68c3f9912e4670485d
Aryandi Putra <aryandi@7langit.com>
2018-02-11 17:11:33 +0700

    [ADD] toolbar on add contact activity

--- app/src/main/java/id/ryandzhunter/contact/ui/addcontact/AddContactActivity.kt
+++ app/src/main/java/id/ryandzhunter/contact/ui/addcontact/AddContactActivity.kt
@@ -2,12 +2,18 @@ package id.ryandzhunter.contact.ui.addcontact
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import id.ryandzhunter.contact.R
 import id.ryandzhunter.contact.di.module.GlideApp
 import id.ryandzhunter.contact.model.Contact
 import kotlinx.android.synthetic.main.activity_add_contact.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by ryandzhunter on 2/10/18.
@@ -17,8 +23,18 @@ class AddContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
+        setToolbar()
         var contact = intent.getParcelableExtra<Contact>(INTENT_CONTACT)
         initView(contact)
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.title = getString(R.string.toolbar_add_title)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat
+                .getColor(this, R.color.detailSecondary)))
     }
 
     private fun initView(contact: Contact?) {
@@ -32,6 +48,27 @@ class AddContactActivity : AppCompatActivity() {
         editLastName.setText(contact?.lastName)
         editPhone.setText(contact?.phoneNumber)
         editEmail.setText(contact?.email)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+            R.id.toolbar_check -> {
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
